9b8b6b937e9d86a56a22399280d5d07d191e6a6e
Folajomi1 <adesolaadeshina112@outlook.com>
2023-04-16 10:03:29 -0700

    my first commit

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,1 +1,5 @@
-DROP DATABASE IF EXISTS hbtn_0c_0;
+#!/usr/bin/env bash
+
+# This script deletes the database hbtn_0c_0 in the MySQL server
+
+mysql -uroot -p -e "DROP DATABASE IF EXISTS hbtn_0c_0;"
