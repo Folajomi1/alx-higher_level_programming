f3fb7107563860a4b89881e32a13e216aa23e829
Folajomi1 <adesolaadeshina112@outlook.com>
2023-04-22 04:33:08 -0700

    my first commit

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,7 +1,5 @@
 -- Script that lists all the tables of a database in MySQL server
-SELECT table_name -- Select the table_name column from the information_schema.tables table
-FROM information_schema.tables -- Get all tables from the information_schema database
-WHERE table_schema = DATABASE() -- Filter by the current database
-ORDER BY table_name; -- Sort the table names alphabetically
-
--- End of script
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = DATABASE()
+ORDER BY table_name; 
