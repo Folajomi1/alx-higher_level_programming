d0153b2e63bb8ca337d081e9fc239d935850cc38
Folajomi1 <adesolaadeshina112@outlook.com>
2023-05-01 17:52:57 -0700

    my first commit

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+
+const sortedDict = {};
+for (const userId in dict) {
+  const occurrences = dict[userId];
+  if (occurrences in sortedDict) {
+    sortedDict[occurrences].push(userId);
+  } else {
+    sortedDict[occurrences] = [userId];
+  }
+}
+
+console.log(sortedDict);
