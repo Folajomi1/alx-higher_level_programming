023dd2a033bc1553de78e09db2e0cb6fcac2b3a1
Folajomi1 <adesolaadeshina112@outlook.com>
2023-05-05 10:15:31 -0700

    new changes

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-
-class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      // Create an empty object
-    }
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
-}
-
-module.exports = Rectangle;
+};
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,22 +1,10 @@
 #!/usr/bin/node
-
-class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      // Create an empty object
-    }
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
-  print() {
-    if (this.width && this.height) {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
-    }
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
-}
-
-module.exports = Rectangle;
+};
